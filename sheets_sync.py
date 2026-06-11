339ef12528b2c66dcb01379ff9d834e9048ed360
salesdatacenterpro-tech <sales.datacenterpro@gmail.com>
2026-06-11 22:09:27 +0300

    fix base64 padding

--- sheets_sync.py
+++ sheets_sync.py
@@ -14,7 +14,7 @@ socket.getaddrinfo = _ipv4_only
 def get_client():
     b64 = os.environ.get("SERVICE_ACCOUNT_B64")
     if b64:
-        info = json.loads(base64.b64decode(b64).decode())
+        info = json.loads(base64.b64decode(b64 + "==").decode())
     else:
         info = json.load(open(str(Path.home() / "Desktop" / "service_account.json")))
     creds = Credentials.from_service_account_info(info, scopes=["https://www.googleapis.com/auth/spreadsheets"])
